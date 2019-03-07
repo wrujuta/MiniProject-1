ad5a066e27715f179cc5eec5a9adb9a676b7d59c
wrujuta <wpd2@njit.edu>
2019-03-06 22:21:19 -0500

    Feature: Using a Factory (in progress)

--- public/index.php
+++ public/index.php
@@ -6,6 +6,72 @@
  * Time: 4:42 PM
  */
 
-echo 'Welcome to IS601';
+main::start();
 
-echo 'test'; 
+class main {
+
+    static public function start() {
+        $records = csv::getRecords();
+        $table = html::generateTable($records);
+
+        system::printPage($table);
+
+    }
+
+
+}
+
+class csv {
+
+    static public function getRecords() {
+
+        $make = 'ford';
+        $model = 'taurus'
+       $car = AutomobileFactory::create($make, $model);
+        $records[] = $car;
+        print_r($records);
+        return $records;
+    }
+}
+
+class html{
+
+    static public function generateTable($records) {
+
+        $table = $records;
+        return $table;
+    }
+}
+
+class system{
+
+    static public function printPage($page) {
+
+        echo $page;
+    }
+}
+
+class Automobile
+{
+    private $vehicleMake;
+    private $vehicleModel;
+
+    public function __construct($make, $model)
+    {
+        $this->vehicleMake = $make;
+        $this->vehicleModel = $model;
+    }
+
+    public function getMakeAndModel()
+    {
+        return $this->vehicleMake . ' ' . $this->vehicleModel;
+    }
+}
+
+class AutomobileFactory
+{
+    public static function create($make, $model)
+    {
+        return new Automobile($make, $model);
+    }
+}
